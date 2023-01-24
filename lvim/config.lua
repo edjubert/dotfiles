7ecef6c3bffb40f35fbd592e35e04fe4d0df98e7
edjubert <edouard.jubert@gmail.com>
2023-01-24 15:44:57 +0100

    new column

--- lvim/config.lua
+++ lvim/config.lua
@@ -455,3 +455,7 @@ BufferOptions = {
 
 
 require 'winpick'.setup({})
+
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum .'' : v:lnum) : ''}%=%s"
