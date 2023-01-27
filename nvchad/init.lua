8571b484f53898ce87bd6c6e94110633d59cc7c1
edjubert <edouard.jubert@gmail.com>
2023-01-27 17:57:37 +0100

    only push custom config

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,34 +1,18 @@
-vim.defer_fn(function()
-  pcall(require, "impatient")
-end, 0)
+local autocmd = vim.api.nvim_create_autocmd
 
-require "core"
-require "core.options"
+-- Auto resize panes when resizing nvim window
+autocmd("VimResized", {
+	pattern = "*",
+	command = "tabdo wincmd =",
+})
 
--- setup packer + plugins
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+autocmd("BufWritePre", {
+	pattern = "*",
+	command = "lua vim.lsp.buf.formatting()",
+})
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  print "Cloning packer .."
-  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- install plugins + compile their configs
-  vim.cmd "packadd packer.nvim"
-  require "plugins"
-  vim.cmd "PackerSync"
-
-  -- install binaries from mason.nvim & tsparsers
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "PackerComplete",
-    callback = function()
-      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
-      require("packer").loader "nvim-treesitter"
-    end,
-  })
-end
-
-pcall(require, "custom")
-
-require("core.utils").load_mappings()
+vim.o.guifont = "Iosevka Nerd Font"
+vim.g.smoothie_experimental_mappings = true
+vim.opt.numberwidth = 3
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum .'' : v:lnum) : ''}%=%"
