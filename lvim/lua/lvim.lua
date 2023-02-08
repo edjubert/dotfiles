43d416e9a4515c55e42ba8ee104606119bfc2426
edjubert <edouard.jubert@gmail.com>
2023-02-08 19:26:10 +0100

    upate telescope config

--- lvim/lua/lvim.lua
+++ lvim/lua/lvim.lua
@@ -11,3 +11,13 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+
+lvim.builtin.telescope.pickers.find_files.previewer = nil
+lvim.builtin.telescope.pickers.git_files.previewer = nil
+-- lvim.builtin.telescope.defaults.layout_strategy = "horizontal"
+-- lvim.builtin.telescope.defaults.layout_config.width = 0.8
+
+-- equire('telescope.builtin').find_files({ layout_strategy = 'vertical', layout_config = { width = 0.5 } })
+-- lua
+-- require('telescope.builtin').find_files({ layout_strategy = 'vertical', layout_config = { width = 0.5 } })
