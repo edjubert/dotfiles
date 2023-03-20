b19dee9257b12bc6a3f74aa1e12278551dcb1b52
edjubert <edouard.jubert@gmail.com>
2023-03-20 11:52:34 +0100

    :sparkles:: tokyo_night theme and mouse follow focus (warp)

--- chadwm/chadwm/config.def.h
+++ chadwm/chadwm/config.def.h
@@ -5,11 +5,11 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int default_border =
-    0; /* to switch back to default border after dynamic border resizing via
+    1; /* to switch back to default border after dynamic border resizing via
           keybinds */
 static const unsigned int snap = 32;   /* snap pixel */
-static const unsigned int gappih = 10; /* horiz inner gap between windows */
-static const unsigned int gappiv = 10; /* vert inner gap between windows */
+static const unsigned int gappih = 20; /* horiz inner gap between windows */
+static const unsigned int gappiv = 20; /* vert inner gap between windows */
 static const unsigned int gappoh =
     10; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
@@ -17,16 +17,16 @@ static const unsigned int gappov =
 static const int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning =
-    2; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
+    1; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
           X */
-static const unsigned int systrayspacing = 2; /* systray spacing */
+static const unsigned int systrayspacing = 8; /* systray spacing */
 static const int systraypinningfailfirst =
     1; /* 1: if pinning fails,display systray on the 1st monitor,False: display
           systray on last monitor*/
 static const int showsystray = 1; /* 0 means no systray */
 static const int showbar = 1;     /* 0 means no bar */
 static const int showtab = showtab_auto;
-static const int toptab = 0;   /* 0 means bottom tab */
+static const int toptab = 1;   /* 0 means bottom tab */
 static const int floatbar = 1; /* 1 means the bar will float(don't have
                                   padding),0 means the bar have padding */
 static const int topbar = 1;   /* 0 means bottom bar */
@@ -60,7 +60,7 @@ static const char *fonts[] = {
     "JetBrainsMono Nerd Font Mono:style:medium:size=19"};
 
 // theme
-#include "themes/catppuccin.h"
+#include "themes/tokyo_night.h"
 
 static const char *colors[][3] = {
     /*                     fg       bg      border */
@@ -117,6 +117,7 @@ static const Rule rules[] = {
     // {"Gimp", NULL, NULL, 0, 0, 1, -1},
     {"brave", NULL, NULL, 1 << 8, 0, 0, -1},
     {"eww", NULL, NULL, 0, 0, 1, -1},
+    {"floating_term", NULL, NULL, 0, 0, 1},
 };
 
 /* layout(s) */
@@ -173,9 +174,13 @@ static const Key keys[] = {
     {MODKEY, XK_r, spawn, SHCMD("rofi -show run")},
     {MODKEY, XK_Escape, spawn, SHCMD("rofi -show window")},
     {MODKEY, XK_Return, spawn, SHCMD("alacritty")},
+    {MODKEY | ShiftMask, XK_Return, spawn,
+     SHCMD("alacritty --class floating_term")},
     {MODKEY, XK_w, spawn, SHCMD("brave")},
-    {MODKEY, XK_a, spawn,
+    {MODKEY | ShiftMask, XK_q, spawn,
      SHCMD("/home/edjubert/.config/eww/dashboard/dashboard")},
+    {MODKEY, XK_a, spawn,
+     SHCMD("kill -s USR1 $(pidof deadd-notification-center)")},
     {MODKEY | ShiftMask, XK_v, spawn,
      SHCMD("rofi -modi \"clipboard:greenclip print\" -show clipboard "
            "-run-command \"{cmd}\" --config "
@@ -318,7 +323,6 @@ static const Button buttons[] = {
     {ClkTagBar, 0, Button3, toggleview, {0}},
     {ClkTagBar, MODKEY, Button1, tag, {0}},
     {ClkTagBar, MODKEY, Button3, toggletag, {0}},
-    {ClkTabBar, 0, Button1, focuswin, {0}},
     {ClkTabBar, 0, Button1, focuswin, {0}},
     {ClkTabPrev, 0, Button1, movestack, {.i = -1}},
     {ClkTabNext, 0, Button1, movestack, {.i = +1}},
--- chadwm/chadwm/themes/tokyo_night.h
+++ chadwm/chadwm/themes/tokyo_night.h
@@ -1,13 +1,13 @@
-static const char black[]       = "#1e222a";
-static const char white[]       = "#abb2bf";
+static const char black[]       = "#1d202f";
+static const char white[]       = "#c0caf5";
 static const char gray2[]       = "#2e323a"; // unfocused window border
 static const char gray3[]       = "#545862";
 static const char gray4[]       = "#6d8dad";
-static const char blue[]        = "#61afef";  // focused window border
-static const char green[]       = "#7EC7A2";
-static const char red[]         = "#e06c75";
+static const char blue[]        = "#7dcfff";  // focused window border
+static const char green[]       = "#9ece6a";
+static const char red[]         = "#f7768e";
 static const char orange[]      = "#caaa6a";
-static const char yellow[]      = "#EBCB8B";
-static const char pink[]        = "#c678dd";
+static const char yellow[]      = "#e0af68";
+static const char pink[]        = "#bb9af7";
 static const char col_borderbar[]  = "#1e222a"; // inner border
 
