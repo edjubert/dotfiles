c28069117dce1388d719b2d5fd17f1f661c2052b
edjubert <edouard.jubert@gmail.com>
2023-03-21 19:34:05 +0100

    :zap: update to lxsession

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -15,7 +15,7 @@ picom --config /home/edjubert/.config/picom/picom.conf --experimental-backend &
 
 run dex --autostart --environment dwm
 run /home/edjubert/.config/awesome/scripts/hotplug.sh
-run /usr/lib/polkit-kde-authentication-agent-1
+run /usr/lib/lxsession
 run xss-lock --transfer-sleep-lock -- i3lock --blur 5 --screen 1 --indicator --time-str="%H:%M:%S" --keylayout 0 --time-color='#ffffffee' --date-color='#ffffffee' --layout-color='#ffffffee' --verif-color='#ffffffee' --nofork
 run nm-applet
 run greenclip daemon
