f0f24c1192a6559d50e3e57970eebb9fd692fd32
edjubert <edouard.jubert@gmail.com>
2023-03-21 20:41:19 +0100

    :zap::deadd

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -15,7 +15,7 @@ picom --config /home/edjubert/.config/picom/picom.conf --experimental-backend &
 
 run dex --autostart --environment dwm
 run /home/edjubert/.config/awesome/scripts/hotplug.sh
-run /usr/lib/lxsession
+run /usr/bin/lxsession
 run xss-lock --transfer-sleep-lock -- i3lock --blur 5 --screen 1 --indicator --time-str="%H:%M:%S" --keylayout 0 --time-color='#ffffffee' --date-color='#ffffffee' --layout-color='#ffffffee' --verif-color='#ffffffee' --nofork
 run nm-applet
 run greenclip daemon
@@ -23,4 +23,4 @@ run deadd-notification-center
 # feh --bg-fill /home/edjubert/Workspace/dotfiles/Wallpapers/hiking_in_space.jpg
 
 ~/.config/dwm/scripts/bar.sh &
-while type dwm >/dev/null; do dwm && continue || break; done
+while type dwm >/home/edjubert/dwm.log; do dwm && continue || break; done
