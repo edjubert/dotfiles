8413987f022f4d3591b352c3506767c2ada4e083
edjubert <edouard.jubert@gmail.com>
2024-08-30 15:45:18 +0200

    add fnm to fish

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,3 +1,10 @@
 # fnm
 # set PATH "/home/edjubert/.local/share/fnm" $PATH
 # fnm env --use-on-cd | source
+
+# fnm
+set FNM_PATH "/home/edjubert/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
