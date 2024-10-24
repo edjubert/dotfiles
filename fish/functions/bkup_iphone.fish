a7f770ebf3a93422f3b78637160db23bfd0010da
edjubert <edouard.jubert@gmail.com>
2024-10-24 10:15:50 +0200

    [fish] bkup iphone script switched to rsync

--- fish/functions/bkup_iphone.fish
+++ fish/functions/bkup_iphone.fish
@@ -1,12 +1,12 @@
 function bkup_iphone
-    #ifuse ~/iPhone_mnt/
+    ifuse ~/iPhone_mnt/
 
     echo $argv[1]
-    return
+    #return
 
     mkdir -p $argv[1]/{photos,videos}
-    cp -rfvp ~/iPhone_mnt/DCIM/**/*.{JPG,JPEG,PNG,HEIC,jpg,jpeg,png,heic} $argv[1]/photos
-    cp -rfvp ~/iPhone_mnt/DCIM/**/*.{MOV,MP4,mov,mp4} $argv[1]/videos
+    rsync -av -P ~/iPhone_mnt/DCIM/**/*.{JPG,JPEG,PNG,HEIC,jpg,jpeg,png,heic} $argv[1]/photos
+    rsync -av -P ~/iPhone_mnt/DCIM/**/*.{MOV,MP4,mov,mp4} $argv[1]/videos
 
     ls ~/iPhone_mnt/DCIM/**/*.{JPG,JPEG,PNG,HEIC,jpg,jpeg,png,heic,MOV,MP4,mov,mp4} | wc -l
     ls $argv[1]/{photos,videos} | wc -l
